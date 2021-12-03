a763e719656baa1e73d844f7ebadd8677772d45f
gosmain <gosmain@naver.com>
2021-12-03 18:14:21 +0900

    change css

--- js/js.js
+++ js/js.js
@@ -8,7 +8,7 @@ function toggle(){
 window.addEventListener("scroll", function(){
     let header = document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY > 0);
-}) ;
+}) 
 
 // image scroll
 
